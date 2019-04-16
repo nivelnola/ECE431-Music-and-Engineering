835e23eb0865e357eb83e59310f1fc764bb5ba81
nivelnola <alon.s.levin@gmail.com>
2019-04-16 14:54:37 -0400

    Tremolo finished. Comments and error catching added to tremolo and ringmod.

--- Project 4/ringmod.m
+++ Project 4/ringmod.m
@@ -1,10 +1,16 @@
-
 function [output] = ringmod(constants,inSound,inputFreq,depth)
 %RINGMOD applies ring modulator effect to inSound
 
-lenSound = length(inSound);
-fixedFreq = depth * sin(2 * pi * inputFreq * (0:1/constants.fs:(lenSound-1)/constants.fs)).';
+% Ensure that depth is within range
+if depth > 1 || depth < 0
+    error('ERROR: Depth must be between 0.');
+end
 
-output = [fixedFreq .* inSound];
+% Derive the ringing signal
+lenSound = length(inSound);
+modulator = depth * sin(2 * pi * inputFreq * (0:1/constants.fs:(lenSound-1)/constants.fs)).';
+
+% Modulate the input signal
+output = modulator .* inSound;
 
 end
--- Project 4/tremolo.m
+++ Project 4/tremolo.m
@@ -3,3 +3,25 @@ function [output] = tremolo(constants,inSound,LFO_type,LFO_rate,lag,depth)
 %signal by a low frequency oscillator specified by LFO_type and LFO_rate. 
 % depth determines the prevalence of the tremeloed signal in the output,
 % and lag determines the delay between the left and right tracks. 
+
+lenSound = length(inSound);
+
+% Ensure LFO_rate is within range
+if LFO_rate > 5 || LFO_rate < 0.05
+    error('ERROR: LFO rate must be between 0.05 Hz and 5 Hz.');
+end
+
+switch true
+    case strcmpi(LFO_type, 'sin')
+        modulator = depth * sin(2 * pi * LFO_rate * (lag + (0:1/constants.fs:(lenSound-1)/constants.fs))).';
+    case strcmpi(LFO_type, 'triangle')
+        modulator = depth * sawtooth(2 * pi * LFO_rate * (lag + (0:1/constants.fs:(lenSound-1)/constants.fs)), 0.5).';
+    case strcmpi(LFO_type, 'square')
+        modulator = depth * square(2 * pi * LFO_rate * (lag + (0:1/constants.fs:(lenSound-1)/constants.fs))).';
+    otherwise
+        error('ERROR: Improper LFO type selected: %s.\nMust be one of:\n\t"sin"\n\t"triangle"\n\t"square"', LFO_type);
+end
+
+% Modulate the input signal
+output = modulator .* inSound;
+end
